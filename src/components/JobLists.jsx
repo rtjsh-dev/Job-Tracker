dd1134587ff4bb471dc3d58dc0cd4f4d1432aaba
Rajesh Thapa <okthatsall678@gmail.com>
2025-06-09 20:36:22 +0545

    display section completed

--- src/components/JobLists.jsx
+++ src/components/JobLists.jsx
@@ -1,15 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const JobLists = () => {
-  const [jobs, setJobs] = useState([]);
-
-  useEffect(() => {
-    const storedJobs = localStorage.getItem("jobLists");
-    if (storedJobs) {
-      setJobs(JSON.parse(storedJobs));
-      console.log(jobs);
-    }
-  }, []);
+const JobLists = ({jobs}) => {
 
   return (
     <div className="text-black p-6 space-y-4">
@@ -21,7 +12,7 @@ const JobLists = () => {
             key={job.id}
             className="bg-white p-4 rounded-xl shadow-md space-y-1"
           >
-            <h3 className="text-lg font-semibold">{job.companyName}</h3>
+            <h3 className="text-lg font-semibold">Company: {job.companyName}</h3>
             <p>Position: {job.position}</p>
             <p>Source: {job.source}</p>
           </div>
