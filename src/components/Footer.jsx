6aaa5303714d4c01ae46d2625e4e7a02b2b64bab
rtjsh-dev <rajesh025thapa@gmail.com>
2025-08-03 12:23:10 +0545

    Changes made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     <footer className="border-t border-gray-300 text-center py-4 text-sm text-gray-600 bg-white">
       Made with <span className="text-red-500">❤️</span> by{" "}
       <a
-        href="https://github.com/rtjsh"
+        href="https://github.com/rtjsh-dev"
         target="_blank"
         rel="noopener noreferrer"
         className="underline hover:text-blue-500"
